a67528a29b8bc51f25367857aa7ea7b4b6369478
Ayush-42 <113936464+Ayush-42@users.noreply.github.com>
2023-04-18 17:42:14 +0530

    Update Map.js

--- components/Map.js
+++ components/Map.js
@@ -14,7 +14,7 @@ const Map = () => {
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/ayush0320/clbv84dxk004815qow61aqn7u',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
       center: [-99.29011, 39.39172],
       zoom: 3,
     })
